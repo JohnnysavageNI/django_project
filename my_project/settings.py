b93a79ed7d734a7fa3dca4cf7a121dde1cba7fb3
JohnnysavageNI <johnnysavagedesign@gmail.com>
2025-04-08 19:06:19 +0100

    Prepare for Heroku deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!r+6ta4$buzjm@bxvk!!c3$-708x2ma&_uyw_p0(w^r1w*b(k-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
